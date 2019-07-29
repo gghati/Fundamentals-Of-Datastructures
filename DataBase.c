a7293f43a5ce065479d39cb60d4bd1c6044180cf
Gaurav Ghati <gauravghati225@gmail.com>
2019-07-29 13:02:28 +0530

    3rd commit

--- DataBase.c
+++ DataBase.c
@@ -10,13 +10,6 @@ Batch : F 10
 int i=0;
 const int BASEROLL = 23201; 
 
-void add(struct Student &students[50], int num);      //num = total number of students  
-void display(struct Student &students[50], int num);
-void modify(struct Student &students[50], int roll);  //roll = index of the array
-void delete(struct Student &students[50], int roll);
-void search(struct Student &students[50], int roll);
-void displayMenu();
-
 struct Student{
    int stuRoll;
    char name[20];
@@ -25,62 +18,115 @@ struct Student{
    int mobNo;
 };
 
-void delete(struct Student &students[50], int roll){
-   i =  
-}
+//void add(struct Student students[50], int num);      //num = total number of students  
+//void display(struct Student students[50], int num);
+//void modify(struct Student students[50], int roll);  //roll = index of the array
+//void delete(struct Student students[50], int num, int roll);
+//void search(struct Student students[50], int roll);
+//void displayMenu();
 
-void main(){
+
+int main(){
+   struct Student stu[50];
+   int num = 0;
+   int exit = 0;
+   int roll = 0;
+   int ch;
+   
+   do{
+     displayMenu();
+     printf("\nENTER CHOICE:");
+     scanf("%d", &ch);
+     
+     switch(ch){
+       case 1:
+           add(stu, num);     
+           num++;
+           break;
+           
+       case 2:
+           printf("Which Roll No you want to modify:");
+           scanf("%d", &roll);
+           modify(stu, roll);
+           break;
+           
+       case 3:
+           printf("Which field you want to modify:");
+           scanf("%d", &roll);
+           delete(stu, num, roll);
+           num--;
+           break;
+           
+       case 4:
+           display(stu, num);
+           break;
        
+       case 5:
+          exit = 1;            
+     }    
+   }while(exit!=1);
+   
+   return 0;
 }
 
-
-
-
-
-
+void delete(struct Student students[50], int num, int roll){   //DONE
+   for(int i=0; i<num-1; i++){
+      if(roll>=(BASEROLL+i)){
+         for(int j=0; j<20; j++){
+            students[i].name[j] = students[i+1].name[j];
+            students[i].city[j] = students[i+1].city[j];
+            if(j<2){
+              students[i].marks[j] = students[i+1].marks[j];
+            }
+         }
+         students[i].mobNo;
+      }
+   }
+   students[num-1].stuRoll = 0;
+}
 
 void displayMenu(){   //Done
   printf("\n<-------DataBase Operations------->");
   printf("\n1) Add Student.");
   printf("\n2) Modify Details");
   printf("\n3) Delete Student");
-  printf("\n4) Display Menu");
+  printf("\n4) Display Details");
   printf("\n5) Exit");
 }
 
-void display(struct Student &students[50], int num){       //Done
+void display(struct Student students[50], int num){       //Done
    printf("\n<----------------------------------STUDENT DATABASE------------------------------------->");
    printf("\nRollNo.\tName\tCity\tMob Number\tMarks 1\tMarks 2");
    printf("\n---------------------------------------------------------------------------------------------");
    for(i=0; i<num; i++){
-     printf("%d\t%s\t%s\t%d\t%d\t%d",students[i].stuRoll, students[i].name, students[i].city, students[i].mobNo, students[i].marks[0],students[i].marks[0]);
+     printf("\n%d\t%s\t%s\t%d\t%d\t%d",students[i].stuRoll, students[i].name, students[i].city, students[i].mobNo, students[i].marks[0],students[i].marks[0]);
    }  
    printf("\n---------------------------------------------------------------------------------------------");   
 }
 
-void search(struct Student &students[50], int roll){   //Done
+void search(struct Student students[50], int roll){   //Done
     i = roll-BASEROLL;   
     printf("\nDetails of Student:");
     printf("\n%d\t%s\t%s\t%d\t%d\t%d",students[i].stuRoll, students[i].name, students[i].city, students[i].mobNo, students[i].marks[0],students[i].marks[0]);
 }
 
-void modify(struct Student &students[50], int roll){         //Done
+void modify(struct Student students[50], int roll){         //Done
      i = roll-BASEROLL;
      int ch = 0;
-     printf("what you wannna Update:\n1)Name\n2)City\n3)Mobile Number\n4)Marks 1");
+     printf("what you wannna Update:\n1)Name\n2)City\n3)Mobile Number\n4)Marks");
      scanf("%d",&ch);
      
      switch(ch){
        case 1: printf("\nUPDATE the Name of Student:");
-               scanf("%s",&students[i].name);
+               scanf("%s", students[i].name);
                break;
                
        case 2: printf("\nUPDATE the City:");
-               scanf("%s",&students[i].city);
+               scanf("%s", students[i].city);
                break;
                
        case 3: printf("\nUPDATE the Mobile NUmber:");
-               scanf("%s",&students[i].mobNo);
+               scanf("%d",&students[i].mobNo);
                break;
                
        case 4: printf("\nUPDATE the Marks of students of previous year:");
@@ -91,13 +137,14 @@ void modify(struct Student &students[50], int roll){         //Done
 }
 
 
-void add(struct Student &students[50], int num){     // num = total number of student present in database  //Done
+void add(struct Student students[50], int num){     // num = total number of student present in database  //Done
+   students[num].stuRoll = BASEROLL + num;
    printf("\nEnter the Name of Student:");
-   scanf("%s",&students[num-1].name);
+   scanf("%s",students[num].name);
    printf("\nEnter the City:");
-   scanf("%s",&students[num-1].city);
+   scanf("%s",students[num].city);
    printf("\nEnter the Mobile NUmber:");
-   scanf("%s",&students[num-1].mobNo);
+   scanf("%d",&students[num].mobNo);
    printf("\nEnter the Marks of students of previous year:");
-   scanf("%d %d",&students[num-1].marks[0], &students[num-1].marks[1]);  
+   scanf("%d %d",&students[num].marks[0], &students[num].marks[1]);  
 } 
